1610956454e70e399bcfa82a895d43ae490bf530
Nami Eskandarian <u1117098@utah.edu>
2022-11-18 14:24:33 -0700

    Wire class basic implementation without gates

--- A8_EducationalApp/wire.cpp
+++ A8_EducationalApp/wire.cpp
@@ -1,6 +1,19 @@
 #include "wire.h"
 
-wire::wire()
-{
+#include <QPen>
 
+// Constructor for wire class
+Wire::Wire(QGraphicsItem *parent) : QGraphicsLineItem(parent) {
+    value = 0; // Preset wire's value to 0, won't care until update time
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
+    setPen(QPen(myColor, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+}
+
+// Updates value held on wire and updates entire circuit
+void Wire::updateValue(bool newValue) {
+
+    value = newValue;
+
+    // When this wire's value changes, all subsequent gate's outputs will change
+    // LOOP THROUGH ENDS AND UPDATE THOSE GATES
 }
--- A8_EducationalApp/wire.h
+++ A8_EducationalApp/wire.h
@@ -1,12 +1,24 @@
 #ifndef WIRE_H
 #define WIRE_H
 
-#include <QObject>
+#include <QGraphicsLineItem>
 
-class wire
+class Wire : public QGraphicsLineItem
 {
 public:
-    wire();
+    Wire(QGraphicsItem *parent = nullptr); // Need to include gate classes in constructor (begin, end)
+
+    void updateValue(bool newValue); // Updates this wire's value, then updates the next gates
+    bool getValue() const { return value; }
+    // Connect method will connect the end of this wire to another gate
+    // Disconnect method will disconnect the end of this wire from the desired gate
+
+private:
+    // Gate pointer "begin" for what this wire starts from
+    // Gate pointer list "ends" for what this wire ends at
+    bool value; // Each wire has a value of true or false (1 or 0)
+    QPolygonF beginNode, endNode;
+    QColor myColor = Qt::black;
 };
 
 #endif // WIRE_H
