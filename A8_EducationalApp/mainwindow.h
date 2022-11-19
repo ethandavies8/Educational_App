3d10191f3b953f54ef7b5c116683bbeee5d98e8d
izeek182 <izeek182@gmail.com>
2022-11-19 11:42:56 -0700

    added box 2d src
    Just added the src no implementation here

--- A8_EducationalApp/mainwindow.h
+++ A8_EducationalApp/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <Box2D/Box2D.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,12 +11,24 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+public slots:
+    void PressedPlay();
+    void PressedInfo();
+    void Pressed();
+private slots:
+    void UpdateWorld();
 private:
     Ui::MainWindow *ui;
+    void titleFallOut();
+    void setUpTitle();
+    void titleFallIn();
+    b2World* world;
+    b2Body* body;
+    float32 timeStep;
+    int32 velocityIterations;
+    int32 positionIterations;
 };
 #endif // MAINWINDOW_H
