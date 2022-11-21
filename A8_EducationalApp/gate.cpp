85403cdd165f931c1e9f7f519c06cc1a128789dd
izeek182 <izeek182@gmail.com>
2022-11-21 15:46:18 -0700

    Finished the patch

--- A8_EducationalApp/gate.cpp
+++ A8_EducationalApp/gate.cpp
@@ -1,16 +1,17 @@
 #include "gate.h"
+#include "wire.h"
 
 //Constructors for gates.
 Gate::Gate(){
 }
 
 //Normal gates can have any number of inputs
-void Gate::addInput(Wire wire){
+void Gate::addInput(Wire* wire){
     inputs.push_back(wire);
 }
 
 //Not gates can only have one input
-void NOTGate::addInput(Wire wire){
+void NOTGate::addInput(Wire* wire){
     if(inputs.size() == 0)
         inputs.push_back(wire);
 }
@@ -18,55 +19,55 @@ void NOTGate::addInput(Wire wire){
 
 //CONSTRUCTOR FOR SOURCE GATE, OUTPUT ALWAYS TRUE
 SourceGate::SourceGate(){
-    outputWire.updateValue(true);
+    outputWire->updateValue(true);
 }
 
 //SET OUTPUTS FOR GATE TYPES<<<<<
 void NOTGate::setOutput(){
-    outputWire.updateValue(!inputs[0].getValue());
+    outputWire->updateValue(!inputs[0]->getValue());
 }
 void ANDGate::setOutput(){
     bool tempLogic;
     if(inputs.size() >= 2){
-        tempLogic = inputs[0].getValue() && inputs[1].getValue();
+        tempLogic = inputs[0]->getValue() && inputs[1]->getValue();
         for(int i = 2; i < (int)inputs.size(); i++)
-            tempLogic = tempLogic & inputs[i].getValue();
-        outputWire.updateValue(tempLogic);
+            tempLogic = tempLogic & inputs[i]->getValue();
+        outputWire->updateValue(tempLogic);
     }
     else
-        outputWire.updateValue(false);
+        outputWire->updateValue(false);
 }
 //The only difference between the NANDGate setOutput is the "!" after the for loop
 void NANDGate::setOutput(){
     bool tempLogic;
     if(inputs.size() >= 2){
-        tempLogic = inputs[0].getValue() && inputs[1].getValue();
+        tempLogic = inputs[0]->getValue() && inputs[1]->getValue();
         for(int i = 2; i < (int)inputs.size(); i++)
-            tempLogic = tempLogic & inputs[i].getValue();
-        outputWire.updateValue(!tempLogic);
+            tempLogic = tempLogic & inputs[i]->getValue();
+        outputWire->updateValue(!tempLogic);
     }
     else
-        outputWire.updateValue(false);
+        outputWire->updateValue(false);
 }
 void ORGate::setOutput(){
     bool tempLogic;
     if(inputs.size() >= 2){
-        tempLogic = inputs[0].getValue() || inputs[1].getValue();
+        tempLogic = inputs[0]->getValue() || inputs[1]->getValue();
         for(int i = 2; i < (int)inputs.size(); i++)
-            tempLogic = tempLogic | inputs[i].getValue();
-        outputWire.updateValue(tempLogic);
+            tempLogic = tempLogic | inputs[i]->getValue();
+        outputWire->updateValue(tempLogic);
     }
     else
-        outputWire.updateValue(false);
+        outputWire->updateValue(false);
 }
 void XORGate::setOutput(){
     bool tempLogic;
     if(inputs.size() >= 2){
-        tempLogic = inputs[0].getValue() ^ inputs[1].getValue();
+        tempLogic = inputs[0]->getValue() ^ inputs[1]->getValue();
         for(int i = 2; i < (int)inputs.size(); i++)
-            tempLogic = tempLogic ^ inputs[i].getValue();
-        outputWire.updateValue(tempLogic);
+            tempLogic = tempLogic ^ inputs[i]->getValue();
+        outputWire->updateValue(tempLogic);
     }
     else
-        outputWire.updateValue(false);
+        outputWire->updateValue(false);
 }
