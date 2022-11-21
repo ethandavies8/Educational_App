827ec87a138484c56f1ff209954e4e30b88c5bf9
Nami Eskandarian <u1117098@utah.edu>
2022-11-21 14:07:28 -0700

    Edited wire classes, needs more work soon

--- A8_EducationalApp/wire.h
+++ A8_EducationalApp/wire.h
@@ -2,20 +2,21 @@
 #define WIRE_H
 
 #include <QGraphicsLineItem>
+#include "gate.h"
 
 class Wire : public QGraphicsLineItem
 {
 public:
-    Wire(QGraphicsItem *parent = nullptr); // Need to include gate classes in constructor (begin, end)
+    Wire(Gate& beginGate, Gate& endGate, QGraphicsItem *parent = nullptr); // Need to include gate classes in constructor (begin, end)
 
     void updateValue(bool newValue); // Updates this wire's value, then updates the next gates
     bool getValue() const { return value; }
-    // Connect method will connect the end of this wire to another gate
-    // Disconnect method will disconnect the end of this wire from the desired gate
+    void connect(Gate& gate);
+    void disconnect(Gate& gate);
 
 private:
-    // Gate pointer "begin" for what this wire starts from
-    // Gate pointer list "ends" for what this wire ends at
+    Gate *begin;
+    QVector<Gate*> ends;
     bool value; // Each wire has a value of true or false (1 or 0)
     QPolygonF beginNode, endNode;
     QColor myColor = Qt::black;
