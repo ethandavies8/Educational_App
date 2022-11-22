2b1cf9f317aa3582018fea7c7a737e4782a0986d
battl <battlecast33@gmail.com>
2022-11-21 17:52:34 -0700

    Made setOutput public

--- A8_EducationalApp/gate.h
+++ A8_EducationalApp/gate.h
@@ -10,11 +10,11 @@ class Gate
 protected:
     std::vector<Wire*> inputs;
     Wire* outputWire;
-    virtual void setOutput() = 0;
 
 public:
     Gate();
     void addInput(Wire* wire);
+    virtual void setOutput() = 0;
 };
 
 //A source gate doesn't inherit from gate and output is true;
