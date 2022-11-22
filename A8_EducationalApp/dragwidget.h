00e38df864c2d65142be683f3301bcc7b8aa0ef1
Ethan Davies <ethandavies8@gmail.com>
2022-11-22 12:03:27 -0700

    cleanup

--- A8_EducationalApp/dragwidget.h
+++ A8_EducationalApp/dragwidget.h
@@ -1,5 +1,3 @@
-// Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #ifndef DRAGWIDGET_H
 #define DRAGWIDGET_H
@@ -12,7 +10,7 @@ class QDragEnterEvent;
 class QDropEvent;
 QT_END_NAMESPACE
 
-//! [0]
+
 class DragWidget : public QFrame
 {
     Q_OBJECT
@@ -34,6 +32,5 @@ protected:
     void dropEvent(QDropEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
 };
-//! [0]
 
 #endif // DRAGWIDGET_H
