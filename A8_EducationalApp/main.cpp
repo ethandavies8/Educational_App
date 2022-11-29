72dd4ecd5cf468b0a7249d65791d874a3f9f5f2c
Nami Eskandarian <u1117098@utah.edu>
2022-11-29 15:03:38 -0700

    Wrote skeleton code for testing wires and gates

--- A8_EducationalApp/main.cpp
+++ A8_EducationalApp/main.cpp
@@ -1,9 +1,21 @@
 #include "mainwindow.h"
 
+// These are used for testing and debugging
+#include <iostream>
+#include "gate.h"
+#include "wire.h"
+
 #include <QApplication>
+
+// Testing function for debugging purposes
+bool testGatesAndWires() {
+    return true;
+}
+
 
 int main(int argc, char *argv[])
 {
+    std::cout << testGatesAndWires() << std::endl;
     srand(time(0));
     QApplication a(argc, argv);
     MainWindow w;
